ff14e6cad6f88c80b1b882d4df6e3f881bd1df31
CristianCaro915 <c.caro@uniandes.edu.co>
2022-10-12 18:20:07 -0700

    commit open .in

--- ProblemaP1.py
+++ ProblemaP1.py
@@ -1,1 +1,18 @@
 #c.caro rama prueba
+from os import strerror
+
+try:
+    ccnt = lcnt = 0
+    stream = open("P1.in", "rt", encoding = "utf-8")
+    line = stream.readline()
+    while line != '':
+        lcnt += 1
+        for ch in line:
+            print(ch, end='')
+            ccnt += 1
+        line = stream.readline()
+    stream.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
