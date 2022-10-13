7e03732527dbdbf9e6cc667d5c5bbd4a656d6bb6
s-umanap <69535816+s-umanap@users.noreply.github.com>
2022-10-13 05:30:32 -0500

    Update ProblemaP1.py

--- ProblemaP1.py
+++ ProblemaP1.py
@@ -30,3 +30,36 @@ def leer_P1(archivo):
     retorno = retorno[1:longitud+1]
     return(retorno)
 print(leer_P1("P1.in"))
+
+## funciones útiles
+def chain_and_subchain(Chain,Subchain):
+    len_chain = len(Chain);
+    len_subchain = len(Subchain);  
+    apariciones = 0
+    
+    if (len_chain == 0 or len_chain < len_subchain):# Caso base 
+        apariciones = 0;
+        return apariciones;
+
+    if (Chain[0 : len_subchain] == Subchain):#caso recursivo donde se compara slices con la subcadena
+        apariciones = chain_and_subchain(Chain[1:],Subchain) + 1; #se va contando la aparición en el caso
+        return apariciones;
+ 
+    return chain_and_subchain(Chain[1:],Subchain);#Caso:ya no hay más iteraciones contables, se obtiene el total
+
+def first_last(Chain,Subchain):
+    
+    slic = Chain[0]+Chain[-1] #se hace slice del primer caracter y el último
+    inverted = slic[::-1] #se invierte el slice hecho anteriormente
+    apariciones = 0
+    if (Subchain == slic) or (Subchain == inverted): #se compara con el subchain los dos casos
+        apariciones = 1
+    return(apariciones)
+
+
+def total_apariciones(Chain,Subchain): #funcion para sacar el total
+    cadena_sin_modificaciones = chain_and_subchain(Chain,Subchain)
+    primera_ultima = first_last(Chain,Subchain)
+    
+    return(cadena_sin_modificaciones + primera_ultima) #llamar a las funciones que se encargan de contar las apariciones de Y en X.
+
